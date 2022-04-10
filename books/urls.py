3500fd1266b6e4d4be96c753d078ec00a84d1378
Lkacik <cep6@poczta.fm>
2022-04-10 12:38:10 +0200

    cleaing

---
+++ books/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from books.views import get_uuids_a, get_uuids_b, get_image, get_button, get_argument_from_path, \
+    get_argument_from_query, check_http_query_type, get_headers, raise_error_for_fun
+
+urlpatterns = [
+    path('uuid-a', get_uuids_a),
+    path('uuid-b', get_uuids_b),
+    path('image', get_image),
+    path('button', get_button),
+    path('path-args/<int:x>/<str:y>/<slug:z>/', get_argument_from_path, name="get_from_path"),
+    path('path-query', get_argument_from_query, name="get_from_query"),
+    path('path-type', check_http_query_type, name="check_http_query_type"),
+    path('get-headers', get_headers, name="get_headers"),
+    path('raise-error', raise_error_for_fun, name="raise_error_for_fun"),
+
+]
+
