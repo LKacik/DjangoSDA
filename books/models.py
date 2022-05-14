10888338e7142ba9a49f7972ad2ae5860c57c783
Lkacik <cep6@poczta.fm>
2022-05-14 13:28:25 +0200

    Added aplication users

--- books/models.py
+++ books/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth.models import User
 from django.db.models import QuerySet
 
