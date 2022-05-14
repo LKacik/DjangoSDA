e2b0e85c9f4d0de4f84d8f83650492e3f34ca125
Lkacik <cep6@poczta.fm>
2022-05-14 11:25:56 +0200

    repair class update, create, delete

--- books/views.py
+++ books/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
-from django.views.generic import TemplateView, DetailView, ListView, FormView, CreateView, UpdateView
+from django.views.generic import TemplateView, DetailView, ListView, FormView, CreateView, UpdateView, DeleteView
 
 from books.forms import CategoryForm, AuthorForm, BookForm
 from books.models import BookAuthor, Category, Book
@@ -81,15 +81,25 @@ class AuthorUpdateView(UpdateView):
 
 
 class BookCreateView(CreateView):
-    templates_name = 'book_form.html'
+    template_name = "book_form.html"
     form_class = BookForm
-    success_url = reverse_lazy('books_list')
+    success_url =  reverse_lazy("books_list")
 
+# def get_success_url(self):
+#     return reverse_lazy("book_list")
 
 class BookUpdateView(UpdateView):
-    templates_name = 'book_form.html'
+    template_name = "book_form.html"
     form_class = BookForm
-    success_url = reverse_lazy('books_list')
+    success_url = reverse_lazy("books_list")
+
+    def get_object(self, **kwargs):
+        return get_object_or_404(Book, id=self.kwargs.get("pk"))
+
+class BookDeleteView(DeleteView):
+    template_name = "book_delete.html"
+    model = Book
+    success_url = reverse_lazy("books_list")
 
     def get_object(self, **kwargs):
         return get_object_or_404(Book, id=self.kwargs.get("pk"))
